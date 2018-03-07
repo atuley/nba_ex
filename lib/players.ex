f76fdc0dfd1c531ff688ec5224092c0f9626891f
Alex Tuley <alex.intern@gaslight.co>
2018-03-06 23:05:14 -0500

    Refactor find_player_id

--- lib/players.ex
+++ lib/players.ex
@@ -1,10 +1,4 @@
 defmodule NbaEx.Players do
-
-  def find_player_id(last_name, first_name) do
-    all()
-    |> find_player_match("#{last_name}, #{first_name}")
-  end
-
   def all do
     HTTPoison.get!("http://stats.nba.com/stats/commonallplayers/?LeagueID=00&Season=2017-18&IsOnlyCurrentSeason=1", [
                             "user-agent": ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),
@@ -18,12 +12,19 @@ defmodule NbaEx.Players do
   end
   defp get_players(%{"resultSets" => [%{"rowSet" => all_players}]}), do: all_players
 
-  # lowercase names before matching
-  # Use Enum.at?
+  def find_player_id(last_name, first_name) do
+    all()
+    |> find_player_match("#{last_name}, #{first_name}")
+  end
+  
   defp find_player_match([head | tail], formatted_name) do
-    case Enum.find(head, fn(player_name) -> player_name == formatted_name end) do
-      nil -> find_player_match(tail, formatted_name)
-      _ -> head |> List.first
+    current_player_name = Enum.at(head, 1) |> String.downcase
+    searched_player = formatted_name |> String.downcase
+
+    if current_player_name == searched_player do
+      head |> List.first
+    else
+      tail |> find_player_match(formatted_name)
     end
   end
   defp find_player_match([], _formatted_name), do: "Player not found"
