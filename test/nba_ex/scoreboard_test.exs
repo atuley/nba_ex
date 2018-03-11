88d6beefcaf5681bac019264df5e24d502df0e0b
Alex Tuley <alex.intern@gaslight.co>
2018-03-10 21:07:49 -0500

    Make tests better

--- test/nba_ex/scoreboard_test.exs
+++ test/nba_ex/scoreboard_test.exs
@@ -12,7 +12,35 @@ defmodule ScoreboardTest do
     use_cassette "20180308_scoreboard" do
       scoreboard = Scoreboard.get_scoreboard("20180308")
 
+      actual_game = scoreboard.games |> List.first
+      expected_game = %NbaEx.Game{
+              arena: %NbaEx.Arena{
+                city: "Charlotte",
+                country: "USA",
+                name: "Spectrum Center",
+                stateAbbr: "NC"
+              },
+              clock: "",
+              gameId: "0021700972",
+              hTeam: %NbaEx.TeamScore{
+                score: "111",
+                teamId: "1610612766",
+                triCode: "CHA"
+              },
+              period: %NbaEx.GamePeriod{current: 4},
+              startTimeUTC: "2018-03-09T00:00:00.000Z",
+              vTeam: %NbaEx.TeamScore{
+                score: "125",
+                teamId: "1610612751",
+                triCode: "BKN"
+              }
+            }
+
       assert scoreboard.games |> Kernel.length == 5
+      assert actual_game.hTeam.triCode  == expected_game.hTeam.triCode
+      assert actual_game.hTeam.score    == expected_game.hTeam.score
+      assert actual_game.vTeam.triCode  == expected_game.vTeam.triCode
+      assert actual_game.vTeam.score    == expected_game.vTeam.score
     end
   end
 end
--- test/nba_ex/teams_test.exs
+++ test/nba_ex/teams_test.exs
@@ -12,7 +12,21 @@ defmodule TeamsTest do
     use_cassette "teams" do
       teams = Teams.all()
 
+      actual_team = teams |> List.first
+      expected_team = %NbaEx.Team{
+                        city: "Atlanta",
+                        confName: "East",
+                        divName: "Southeast",
+                        fullName: "Atlanta Hawks",
+                        isNBAFranchise: true,
+                        nickname: "Hawks",
+                        teamId: "1610612737",
+                        tricode: "ATL"
+                      }
+
       assert teams |> Kernel.length == 30
+      assert actual_team.teamId   == expected_team.teamId
+      assert actual_team.fullName == expected_team.fullName
     end
   end
 end
