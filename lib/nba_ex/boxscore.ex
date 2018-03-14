60eb0729a0217e7529d48bcc65d277fb4876a5c0
Alex Tuley <alex.intern@gaslight.co>
2018-03-13 23:11:24 -0400

    Refactor boxscore

--- lib/nba_ex/boxscore.ex
+++ lib/nba_ex/boxscore.ex
@@ -1,5 +1,7 @@
 defmodule NbaEx.Boxscore do
-  alias NbaEx.{Constants, Endpoints, Game, PlayerStat, TeamStat}
+  alias NbaEx.{Game, PlayerStat, TeamStat, Utils}
+
+  @endpoint "boxscore.json"
 
   defstruct [
     game: nil,
@@ -8,15 +10,16 @@ defmodule NbaEx.Boxscore do
     player_stats: nil
   ]
 
-  # TODO build url function
-
   def get_boxscore(date, game_id) do
-    HTTPoison.get!("#{Constants.base_url}/#{Constants.base_version}/#{date}/#{game_id}_#{Endpoints.boxscore}").body
+    @endpoint
+    |> Utils.build_url(date, game_id)
+    |> HTTPoison.get!
+    |> Map.get(:body)
     |> Poison.decode!(as: %{"basicGameData" => %Game{}, "stats" => %{"vTeam" => %TeamStat{}, "hTeam" => %TeamStat{}, "activePlayers" => [%PlayerStat{}]}})
-    |> grab_boxscore
+    |> build_boxscore
   end
 
-  defp grab_boxscore(%{"basicGameData" => game,"stats" => %{"vTeam" => away_team_stats, "hTeam" => home_team_stats, "activePlayers" => player_stats}}) do
+  defp build_boxscore(%{"basicGameData" => game,"stats" => %{"vTeam" => away_team_stats, "hTeam" => home_team_stats, "activePlayers" => player_stats}}) do
     %NbaEx.Boxscore{game: game, away_team_stats: away_team_stats, home_team_stats: home_team_stats, player_stats: player_stats}
   end
 end
--- lib/nba_ex/helpers/utils.ex
+++ lib/nba_ex/helpers/utils.ex
@@ -3,8 +3,9 @@ defmodule NbaEx.Utils do
   @base_version "v1/"
   @year         "2017/"
 
-  def build_url("players.json"), do: base_url() <> "players.json"
-  def build_url(_), do: {:error, "Endpoint does not exist"}
+  def build_url("boxscore.json", date, game_id), do: base_url() <> date <> "/#{game_id}_boxscore.json"
+  def build_url("players.json"),                 do: base_url() <> @year <> "players.json"
+  def build_url(_),                              do: {:error, "Endpoint does not exist"}
 
   def current_date do
     Date.utc_today
@@ -12,7 +13,7 @@ defmodule NbaEx.Utils do
   end
 
   def base_url do
-    @base_url <> @base_version <> @year
+    @base_url <> @base_version
   end
 
 
