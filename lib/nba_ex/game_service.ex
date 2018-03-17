05720145ff8eb19be67b9529305faba6dbf0b968
Alex Tuley <alex.intern@gaslight.co>
2018-03-17 13:49:56 -0400

    Mix format + error catching

--- lib/nba_ex/game_service.ex
+++ lib/nba_ex/game_service.ex
@@ -8,33 +8,56 @@ defmodule NbaEx.GameService do
   def get_boxscore(date, game_id) do
     @boxscore
     |> Utils.build_url(date, game_id)
-    |> HTTPoison.get!
+    |> HTTPoison.get!()
     |> Map.get(:body)
-    |> Poison.decode!(as: %{"basicGameData" => %Game{}, "stats" => %{"vTeam" => %TeamStat{}, "hTeam" => %TeamStat{}, "activePlayers" => [%PlayerStat{}]}})
+    |> Poison.decode!(
+      as: %{
+        "basicGameData" => %Game{},
+        "stats" => %{
+          "vTeam" => %TeamStat{},
+          "hTeam" => %TeamStat{},
+          "activePlayers" => [%PlayerStat{}]
+        }
+      }
+    )
     |> build_boxscore
   end
 
   def play_by_play(date, game_id, period) do
     @pbp
     |> Utils.build_url(date, game_id, period)
-    |> HTTPoison.get!
+    |> HTTPoison.get!()
     |> Map.get(:body)
     |> Poison.decode!(as: %PlayByPlay{})
   end
 
   def get_scoreboard(date) do
-    @scoreboard
-    |> Utils.build_url(date)
-    |> HTTPoison.get!
-    |> case do
-        %HTTPoison.Response{body: body, status_code: 200} -> body
-        %HTTPoison.Response{status_code: _} -> raise ArgumentError, "Invalid date for scoreboard"
-       end
-    |> Poison.decode!(as: %Scoreboard{games: [%Game{}]})
+    with %HTTPoison.Response{body: body, status_code: 200} <-
+      @scoreboard
+      |> Utils.build_url(date)
+      |> HTTPoison.get!()
+    do
+      Poison.decode!(body, as: %Scoreboard{games: [%Game{}]})
+    else
+      _ -> {:error, "#{date} is not a valid date. Proper format is: YYYYMMDD"}
+    end
   end
-  def get_scoreboard, do: get_scoreboard(Utils.current_date)
+  def get_scoreboard, do: get_scoreboard(Utils.current_date())
 
-  defp build_boxscore(%{"basicGameData" => game,"stats" => %{"vTeam" => away_team_stats, "hTeam" => home_team_stats, "activePlayers" => player_stats}}) do
-    %NbaEx.Boxscore{game: game, away_team_stats: away_team_stats, home_team_stats: home_team_stats, player_stats: player_stats}
+  defp build_boxscore(%{
+         "basicGameData" => game,
+         "stats" => %{
+           "vTeam" => away_team_stats,
+           "hTeam" => home_team_stats,
+           "activePlayers" => player_stats
+         }
+       })
+  do
+    %NbaEx.Boxscore{
+      game: game,
+      away_team_stats: away_team_stats,
+      home_team_stats: home_team_stats,
+      player_stats: player_stats
+    }
   end
 end
