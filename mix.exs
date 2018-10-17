fb2c357823ed87d77192130df4d3cf843ba35850
Alex Tuley <alex.intern@gaslight.co>
2018-10-17 15:07:19 -0400

    Update package version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NbaEx.MixProject do
     [
       app: :nba_ex,
       description: "Wrapper API for data.nba.net",
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
